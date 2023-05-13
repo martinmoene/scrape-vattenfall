24596e2980144af817b2b4dea324fad7bef2b176
Martin Moene <martin.moene@gmail.com>
2023-05-13 23:50:16 +0200

    Add initial setup for Ada version ('Hello, World!' for now)
    Build: run make in ./ada subdirectory.

---
+++ ada/Readme.md
@@ -0,0 +1,7 @@
+# Ada version of ScrapeVattenfall
+
+Scrape Vattenfall web page with daily electricity usage.
+
+## GNAT Toolset
+
+The GNAT 2021 Community toolset is used, see the [Download GNAT Community Edition](https://www.adacore.com/download) AdaCore web page.
---
+++ ada/src/scrape_electricity_per_day_vattenfall.adb
@@ -0,0 +1,16 @@
+-- Note: 'Hello, World!' placeholder for now to setup Ada environment using GNAT toolset.
+--
+-- Scrape given text file with Vattenfall per-day electricity usage for its entries and create a csv file of it.
+--
+-- Copyright 2023 by Martin Moene
+--
+-- Distributed under the Boost Software License, Version 1.0.
+-- (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure scrape_electricity_per_day_vattenfall is
+
+begin
+   Put_Line ("Hello, World!");
+end scrape_electricity_per_day_vattenfall;
